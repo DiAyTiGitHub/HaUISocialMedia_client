b906c25d6a872f94ae74f42c4e006ab606c1dde9
Dinh Tuan Dat <dtuandat2004@gmail.com>
2024-05-10 09:29:40 +0700

    update leading dashboard FE

--- src/components/LeaderBoard/LeaderBoard.tsx
+++ src/components/LeaderBoard/LeaderBoard.tsx
@@ -1,157 +1,154 @@
-import { useState } from "react";
+import { memo, useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import TableSkeleton from "@/components/skeleton/TableSkeleton";
 import { useStore } from "@/stores";
 import { useGetDataObjectPagination } from "@/lib";
-import Pagination from "./Pagination";
 import Icon from "../shared/Icon";
 import { Input } from "../ui/input";
+import Pagination from '@mui/material/Pagination';
+import { observer } from "mobx-react";
+import DashBoardItem from "./DashBoardItem";
+import "../../common/CommonStyles.scss";
+import { Formik, Form } from "formik";
+import SearchIcon from '@mui/icons-material/Search';
 
-const ItemTable = ({ data, stt }: { data: any; stt: number }) => {
-  const navigate = useNavigate();
-  return (
-    <tr
-      onClick={() => {
-        navigate(`/profile/${data.user.id}`);
-        window.location.href = `/profile/${data.user.id}`;
-      }}
-    >
-      <td className={`px-3 py-2 text-sm text-center `}>
-        <span>{stt}</span>
-      </td>
-
-      <td className="px-4 py-3">
-        <div className="flex justify-center gap-10 text-sm m-0">
-          <div className="relative hidden w-8 h-8 mr-3 rounded-full md:block">
-            <img
-              className="object-cover w-full h-full rounded-full"
-              src={data?.user?.avatar || "/person.jpg"}
-              alt="image"
-            />
-          </div>
-        </div>
-      </td>
-
-      <td className="px-4 py-3 text-sm text-center">
-        {data?.user?.code || "Chưa cập nhật"}
-      </td>
-      <td className="px-4 py-3 text-sm text-center">{data?.user?.lastName}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.user.firstName}</td>
-
-      <td className="px-4 py-3 text-sm text-center">
-        <p className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full">
-          {data.user.username}
-        </p>
-      </td>
-
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfA}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfBPlus}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfB}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfCPlus}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfC}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfDPlus}</td>
-      <td className="px-4 py-3 text-sm text-center">{data.numsOfD}</td>
-    </tr>
-  );
-};
 
 type PagingType = {
   pageSize: number;
   pageIndex: number;
 };
-const LeaderBoard = () => {
+
+function LeaderBoard() {
   const { leaderBoardStore } = useStore();
-  const { getLeadingDashBoard } = leaderBoardStore;
-
-  const [paging, setPaging] = useState<PagingType>({
-    pageSize: 20,
-    pageIndex: 1,
-  });
-
   const {
-    res: leaderBoardData,
+    getLeadingDashBoard,
+    resetStore,
     isLoading,
-    isLeftDisable,
-    isRightDisable,
-  } = useGetDataObjectPagination({
-    getRequest: getLeadingDashBoard,
-    paging: paging,
-  });
+    dashboardData,
+    totalPages,
+    pageIndex,
+    pageSize,
+    handleChangePage,
+    handleSearchingWithKeyword,
+  } = leaderBoardStore;
 
-  const handleSTT = (index: number) => {
-    return paging.pageSize * (paging.pageIndex - 1) + index + 1;
-  };
+  useEffect(function () {
+
+    getLeadingDashBoard();
+
+    return resetStore;
+  }, []);
 
   return (
-    <div className="bg-white p-3 shadow-md rounded-md">
-      <p className="text-heading3-bold">Bảng xếp hạng thành tích</p>
-      <div className="mt-2 flex justify-between">
-        <Input
-          placeholder="Tìm kiếm ..."
-          className="px-4 py-2 rounded-xl w-1/3  outline-none border border-blue-500"
-          disabled
-        />
-        <div className="flex gap-3 items-center">
+    <Formik
+      onSubmit={handleSearchingWithKeyword}
+      enableReinitialize
+      initialValues={{
+        keyword: null
+      }}
+    >
+      {(formProps: any) => {
+        const { values, setFieldValue } = formProps;
+        console.log("values changed: ", values);
+
+        function handleChangeSearchInput(e: any) {
+          setFieldValue("keyword", e?.target?.value);
+        }
+
+        return (
+          <Form className='calendar-form flex justify-center p-4 w-100'>
+            <div className="bg-white p-3 shadow-md rounded-md w-100">
+              <p className="text-heading3-bold">Bảng xếp hạng thành tích</p>
+              <div className="mt-2 flex justify-right">
+                <Input
+                  name="keyword"
+                  placeholder="Tìm kiếm ..."
+                  className="px-4 py-2 rounded-xl w-1/3  outline-none border border-blue-500"
+                  onChange={handleChangeSearchInput}
+                />
+                {/* <div className="flex gap-3 items-center">
           <Button className="flex gap-3 items-center" disabled>
             <Icon name="Filter" />
             <span>Lọc</span>
           </Button>
-        </div>
-      </div>
+        </div> */}
 
-      <div className="w-full overflow-hidden rounded-lg shadow-xs mt-5">
-        <div className="w-full overflow-x-auto ">
-          {isLoading && <TableSkeleton length={5} styles="" />}
-          {!isLoading && (
-            <>
-              <table className="w-full whitespace-nowrap">
-                <thead>
-                  <tr className="text-xs tracking-wide font-bold text-left text-gray-500 uppercase border-b ">
-                    <td className="px-4 py-3 text-center">STT</td>
-                    <td className="px-4 py-3 text-center">Ảnh</td>
-                    <td className="px-4 py-3 text-center">Mã SV</td>
-                    <td className="px-4 py-3 text-center">Họ</td>
-                    <td className="px-4 py-3 text-center">Tên</td>
-                    <td className="px-4 py-3 text-center">Tên người dùng</td>
-                    <td className="px-4 py-3 text-center">A</td>
-                    <td className="px-4 py-3 text-center">B+</td>
-                    <td className="px-4 py-3 text-center">B</td>
-                    <td className="px-4 py-3 text-center">C+</td>
-                    <td className="px-4 py-3 text-center">C</td>
-                    <td className="px-4 py-3 text-center">D+</td>
-                    <td className="px-4 py-3 text-center">D</td>
-                  </tr>
-                </thead>
+                <div className="flex items-center pl-2">
+                  <Button className="flex items-center" type="submit" style={{backgroundColor: "#2d74ff"}}>
+                    <SearchIcon />
+                    <span className="pl-2">Tìm kiếm</span>
+                  </Button>
+                </div>
+              </div>
 
-                <tbody className="bg-white divide-y ">
-                  {leaderBoardData?.data?.map((data: any, index: number) => (
-                    <ItemTable
-                      key={data.id}
-                      data={data}
-                      stt={handleSTT(index)}
-                    />
-                  ))}
-                </tbody>
-              </table>
-              {leaderBoardData?.data?.length === 0 && (
-                <span className="flex justify-center mt-5">
-                  Chưa có dữ liệu
-                </span>
-              )}
-            </>
-          )}
-        </div>
-      </div>
+              <div className="w-full overflow-hidden rounded-lg shadow-xs mt-5">
+                <div className="w-full overflow-x-auto ">
+                  {isLoading && <TableSkeleton length={5} styles="" />}
+                  {!isLoading && (
+                    <>
+                      <table className="w-full whitespace-nowrap">
+                        <thead>
+                          <tr className="text-xs tracking-wide font-bold text-left text-gray-500 uppercase border-b ">
+                            <td className="px-4 py-3 text-center">STT</td>
+                            <td className="px-4 py-3 text-center">Ảnh</td>
+                            <td className="px-4 py-3 text-center">Mã SV</td>
+                            <td className="px-4 py-3 text-center">Họ</td>
+                            <td className="px-4 py-3 text-center">Tên</td>
+                            <td className="px-4 py-3 text-center">Tên người dùng</td>
+                            <td className="px-4 py-3 text-center">A</td>
+                            <td className="px-4 py-3 text-center">B+</td>
+                            <td className="px-4 py-3 text-center">B</td>
+                            <td className="px-4 py-3 text-center">C+</td>
+                            <td className="px-4 py-3 text-center">C</td>
+                            <td className="px-4 py-3 text-center">D+</td>
+                            <td className="px-4 py-3 text-center">D</td>
+                          </tr>
+                        </thead>
 
-      <Pagination
-        isLeftDisable={isLeftDisable}
-        isRightDisable={isRightDisable}
-        setPaging={setPaging}
-      />
-    </div>
+                        <tbody className="bg-white divide-y ">
+                          {dashboardData?.map((data: any, index: number) => (
+                            <DashBoardItem
+                              key={data?.id}
+                              data={data}
+                              stt={((pageIndex - 1) * (pageSize) + index + 1)}
+                            />
+                          ))}
+
+                        </tbody>
+                      </table>
+
+                      {(!dashboardData || dashboardData?.length === 0) && (
+                        <span className="flex justify-center mt-5">
+                          Chưa có dữ liệu
+                        </span>
+                      )}
+
+                      <div className="flex justify-right">
+
+                        <Pagination
+                          count={totalPages}
+                          page={pageIndex}
+                          shape="rounded"
+                          onChange={handleChangePage}
+                          boundaryCount={1}
+                          siblingCount={1}
+                          showFirstButton
+                          showLastButton
+                        />
+                      </div>
+
+                    </>
+                  )}
+                </div>
+              </div>
+            </div>
+          </Form>
+        )
+      }}
+    </Formik>
   );
 };
 
-export default LeaderBoard;
+export default memo(observer(LeaderBoard));
--- src/components/LeaderBoard/LeaderBoardStore.ts
+++ src/components/LeaderBoard/LeaderBoardStore.ts
@@ -7,14 +7,67 @@ class LeaderBoardStore {
     makeAutoObservable(this);
   }
 
-  getLeadingDashBoard = async (searchObject: any) => {
+  dashboardData = [];
+
+  pageIndex = 1;
+  pageSize = 10;
+  keyword = null;
+  isLoading = true;
+  totalPages = 0;
+  totalElements = 0;
+
+
+  getLeadingDashBoard = async () => {
     try {
+      this.isLoading = true;
+      const searchObject = {
+        pageIndex: this.pageIndex,
+        pageSize: this.pageSize,
+        keyWord: this.keyword
+      };
+
       const { data } = await getLeadingDashBoardRequest(searchObject);
-      return data;
+
+      this.dashboardData = data?.data;
+      this.pageIndex = data?.pageIndex;
+      this.pageSize = data?.pageSize;
+      this.keyword = data?.keyword;
+      this.totalElements = data?.totalElements;
+      this.totalPages = data?.totalPages;
+
+      this.isLoading = false;
+
     } catch (error) {
       toast.error("Something went wrong :(");
     }
   };
+
+  handleChangePage = (event: any, newPage: number) => {
+    this.pageIndex = (newPage);
+
+    this.getLeadingDashBoard();
+  };
+
+  handleSearchingWithKeyword = (values: any) => {
+    this.pageIndex = 1;
+    this.keyword = values?.keyword;
+    console.log("submit values: ", values)
+
+    this.getLeadingDashBoard();
+  }
+
+  handleChangeSearchInput = (e: any) => {
+    console.log(e);
+  }
+
+  resetStore = () => {
+    this.dashboardData = [];
+    this.pageIndex = 1;
+    this.pageSize = 10;
+    this.keyword = null;
+    this.isLoading = true;
+    this.totalElements = 0;
+  }
 }
 
 export default LeaderBoardStore;
